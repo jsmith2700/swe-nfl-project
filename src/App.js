07ccccd61b5a8f2e707c8dcb00a71c34cf23a908
brockdetar <75516719+brockdetar@users.noreply.github.com>
2023-03-08 17:36:36 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Sidebar from './Components/Sidebar';
 import image from '../src/NFL Project Background.png'
 import { SliderData } from './Components/SliderData';
 import ImageSlider from './Components/ImageSlider';
+import info from './Components/info';
+
+
+
 
 function App() {
   return (  
       <div class="bg">
-        <div class="navigation"></div>
+        <div class="infobutton">
+          <info info={info}>
+          </info>
+        </div>
         <div class="centerBox">
           <div class="teamText">
             Select Two Teams to Compare Statistics
@@ -17,6 +24,7 @@ function App() {
             <div class="teamSelect">
               <ImageSlider slides={SliderData}/>
             </div>
+            <div class="middleBox">vs</div>
             <div class="teamSelectRight">
               <ImageSlider slides={SliderData}/>
             </div>
