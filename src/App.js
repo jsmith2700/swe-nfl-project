646c73d01534a87cb163dcbc443c54ac2a20dc4d
Brock DeTar <brockdetar@gmail.com>
2023-03-08 22:41:27 -0600

    Updated with information popups

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Sidebar from './Components/Sidebar';
 import image from '../src/NFL Project Background.png'
 import { SliderData } from './Components/SliderData';
 import ImageSlider from './Components/ImageSlider';
-import info from './Components/info';
 
 
 
@@ -12,10 +11,6 @@ import info from './Components/info';
 function App() {
   return (  
       <div class="bg">
-        <div class="infobutton">
-          <info info={info}>
-          </info>
-        </div>
         <div class="centerBox">
           <div class="teamText">
             Select Two Teams to Compare Statistics
